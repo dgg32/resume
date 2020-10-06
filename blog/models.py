5d2f1381ff32be488b44e44687e3ddd83a8f6011
Sixing Huang <email@example.com>
2020-10-06 13:46:21 +0200

    finish the first draft

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,14 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return " ".join(self.body.split(" ")[:10]) + " ..."
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
 # add the blog app to the settings
 
 # create a migration
